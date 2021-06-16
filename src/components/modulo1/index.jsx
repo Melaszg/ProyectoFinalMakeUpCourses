4355e83791ddd118ebd705c293d92ce97500f357
Melaszg <msalazar938@misena.edu.co>
2021-06-15 21:00:31 -0500

    videos

--- src/components/modulo1/index.jsx
+++ src/components/modulo1/index.jsx
@@ -11,13 +11,13 @@ function Modulo1() {
           <div
             className="col-md-6 col-sm-12 flex-wrap"
           >
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/mefnAtifKps" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/wPGzvEDiUWQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <p className="textos text-justify">Debemos tener presente que no todas las brochas son iguales y tienen la misma función. Por ello, en este video te daremos una breve explicación sobre los tipos tipos de brochas, sus usos y funciones que cada una tiene, para que así puedas ampliar tu conocimiento y conocer más sobre estas.</p>
           </div>
           <div
             className="col-md-6 col-sm-12 flex-wrap"
           >
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/r3hqAodNN7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/RD2oZP0Eutw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <p className="textos text-justify">Cada persona tiene una rutina diferente a la hora de maquillarse, pero debemos tener una buena preparación y un buen orden para maquillarnos correctamente. 
 En este video te enseñaremos una rutina de maquillaje diario, sencillo de hacer y con materiales que pueden estar a tu alcance.</p>
           </div>
--- src/components/modulo2/index.jsx
+++ src/components/modulo2/index.jsx
@@ -11,13 +11,13 @@ function Modulo2() {
           <div
             className="col-md-6 col-sm-12 flex-wrap"
           >
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/YdZALIRj6e4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/QqoyTi9wjVU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            <p className="textos text-justify">En esta rutina facial de día te mostraremos el paso a paso de una buena limpieza para el rostro y la realización de una mascarilla exfoliante con productos caseros.</p>
           </div>
           <div
             className="col-md-6 col-sm-12 flex-wrap"
           >
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/h_4adSsMlLI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/h4me536seuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            <p className="textos text-justify">En este video aprenderás a cuidar tu rostro en la noche correctamente, te enseñaremos una buena limpieza y aprenderás qué productos son buenos pare esta.</p>
           </div>
         </div>
